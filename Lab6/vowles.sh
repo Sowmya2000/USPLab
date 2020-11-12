e92ed19ff8cd8c1831d7b18a6c3c09d343793730
Sowmya2000 <55014801+Sowmya2000@users.noreply.github.com>
2020-11-12 14:52:53 +0530

    Add files via upload

---
+++ Lab6/vowles.sh
@@ -0,0 +1,8 @@
+echo -n "Enter a line of text: "
+read string
+
+numCount=$(echo $string | grep -o "[0-9]" | wc --lines)
+vowCount=$(echo $string | grep -o -i "[aeiou]" | wc --lines)
+consCount=$(echo $string | grep -o -i "[bcdfghjklmnpqrstvwxyz]" | wc --lines)
+
+echo "The given string has $vowCount vowels, $consCount consonants and $numCount numbers"
